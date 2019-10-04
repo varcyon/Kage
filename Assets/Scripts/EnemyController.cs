2dbe398ab740658bb69b9da219e9c4ade5e83f8e
varcyon <darkenedmagi@gmail.com>
2019-10-04 16:27:09 -0500

    stop following and enemy type works

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class EnemyController : PhysicsObject
 {
+    [SerializeField] enum EnemyType { Walker, Aggressive };
+    [SerializeField] EnemyType enemyType;
     Damageable damageable;
     [SerializeField] float followRange = 8f;
     [SerializeField] float turnBackRange = 5.0f;
     [SerializeField] float ledgeDetectionDepth = 3f;
-    [SerializeField]  float ledgeDetectionWidth = 2f;
-     float directionSmooth = 1f;
-     float direction = 1;
-     float playerDifference;
-     float changeDirectionEase = 1;
-     bool followPlayer;
+    [SerializeField] float ledgeDetectionWidth = 2f;
+    float directionSmooth = 1f;
+    float direction = 1;
+    float playerDifference;
+    float changeDirectionEase = 2f;
+    bool followPlayer;
     [SerializeField] float maxSpeed = 7;
     [SerializeField] int jumpTakeOffSpeed = 20;
-     RaycastHit2D ground;
-     RaycastHit2D rightWall;
-     RaycastHit2D leftWall;
-     RaycastHit2D rightLedge;
-     RaycastHit2D leftLedge;
-    [SerializeField]  LayerMask groundLayerMask;
+    RaycastHit2D ground;
+    RaycastHit2D rightWall;
+    RaycastHit2D leftWall;
+    RaycastHit2D rightLedge;
+    RaycastHit2D leftLedge;
+    [SerializeField] LayerMask groundLayerMask;
 
-    
+
 
     void OnDrawGizmosSelected()
     {
@@ -32,7 +34,7 @@ public class EnemyController : PhysicsObject
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, turnBackRange);
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, new Vector2(ledgeDetectionWidth*2,ledgeDetectionDepth*2));
+        Gizmos.DrawWireCube(transform.position, new Vector2(ledgeDetectionWidth * 2, ledgeDetectionDepth * 2));
     }
     void Start()
     {
@@ -42,99 +44,109 @@ public class EnemyController : PhysicsObject
 
     protected override void ComputeVelocity()
     {
-
+        Vector2 move = Vector2.zero;
         playerDifference = Player.Instance.gameObject.transform.position.x - transform.position.x;
         directionSmooth += (direction - directionSmooth) * Time.deltaTime * changeDirectionEase;
-
-        ground = Physics2D.Raycast(transform.position, new Vector2(0, -1), 2.2f, groundLayerMask);
-        Debug.DrawRay(transform.position, new Vector2(0, -2.2f), Color.blue);
-
-        if ((Mathf.Abs(playerDifference) < followRange))
+        if (true)
         {
-            followPlayer = true;
-        }
-        else
-        {
-            followPlayer = false;
-        }
-
-
-        if (followPlayer)
-        {
-            if (playerDifference < 0)
+            move.x = 1 * directionSmooth;
+            ground = Physics2D.Raycast(transform.position, new Vector2(0, -1), 2.2f, groundLayerMask);
+            Debug.DrawRay(transform.position, new Vector2(0, -2.2f), Color.blue);
+            if (enemyType == EnemyType.Aggressive)
             {
-                direction = -1;
+                if ((Mathf.Abs(playerDifference) < followRange))
+                {
+                    followPlayer = true;
+                }
+                else
+                {
+                    followPlayer = false;
+                }
+            }
+
+
+
+            if (followPlayer)
+            {
+                if (playerDifference < 0)
+                {
+                    direction = -1;
+                }
+                else
+                {
+                    direction = 1;
+                }
             }
             else
             {
-                direction = 1;
+                directionSmooth = -direction;
             }
-        }
-        else
-        {
-            directionSmooth = direction;
-        }
 
-        //Check for walls
-        rightWall = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.right, turnBackRange, groundLayerMask);
-        Debug.DrawRay(new Vector2(transform.position.x, transform.position.y), new Vector2(turnBackRange, 0), Color.red);
+            //Check for walls
+            rightWall = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.right, turnBackRange, groundLayerMask);
+            Debug.DrawRay(new Vector2(transform.position.x, transform.position.y), new Vector2(turnBackRange, 0), Color.red);
 
-        if (rightWall.collider != null)
-        {
+            if (rightWall.collider != null)
+            {
+                if (!followPlayer)
+                {
+                    direction = 1;
+                }
+                else
+                {
+                    Jump();
+                }
+
+            }
+
+            leftWall = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.left, turnBackRange, groundLayerMask);
+            Debug.DrawRay(new Vector2(transform.position.x, transform.position.y), new Vector2(-turnBackRange, 0), Color.blue);
+
+            if (leftWall.collider != null)
+            {
+                if (!followPlayer)
+                {
+                    direction = -1;
+                }
+                else
+                {
+                    Jump();
+                }
+            }
+
+
+            //Check for ledges
             if (!followPlayer)
             {
-                direction = -1;
+                rightLedge = Physics2D.Raycast(new Vector2(transform.position.x + ledgeDetectionWidth, transform.position.y), Vector2.down, ledgeDetectionDepth);
+                Debug.DrawRay(new Vector2(transform.position.x + ledgeDetectionWidth, transform.position.y), new Vector2(0, -ledgeDetectionDepth), Color.blue);
+                if (rightLedge.collider == null)
+                {
+                    direction = 1;
+                }
+
+                leftLedge = Physics2D.Raycast(new Vector2(transform.position.x - ledgeDetectionWidth, transform.position.y), Vector2.down, ledgeDetectionDepth);
+                Debug.DrawRay(new Vector2(transform.position.x - ledgeDetectionWidth, transform.position.y), new Vector2(0, -ledgeDetectionDepth), Color.blue);
+
+                if (leftLedge.collider == null)
+                {
+                    direction = -1;
+                }
             }
-            else
-            {
-                Jump();
-            }
+            targetVelocity = move * maxSpeed;
 
         }
+        
 
-        leftWall = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.left, turnBackRange, groundLayerMask);
-        Debug.DrawRay(new Vector2(transform.position.x, transform.position.y), new Vector2(-turnBackRange, 0), Color.blue);
-
-        if (leftWall.collider != null)
-        {
-            if (!followPlayer)
-            {
-                direction = 1;
-            }
-            else
-            {
-                Jump();
-            }
-        }
-
-
-        //Check for ledges
-        if (!followPlayer)
-        {
-            rightLedge = Physics2D.Raycast(new Vector2(transform.position.x + ledgeDetectionWidth, transform.position.y), Vector2.down, ledgeDetectionDepth);
-            Debug.DrawRay(new Vector2(transform.position.x + ledgeDetectionWidth, transform.position.y), new Vector2(0, -ledgeDetectionDepth), Color.blue);
-            if (rightLedge.collider == null)
-            {
-                direction = -1;
-            }
-
-            leftLedge = Physics2D.Raycast(new Vector2(transform.position.x - ledgeDetectionWidth, transform.position.y), Vector2.down, ledgeDetectionDepth);
-            Debug.DrawRay(new Vector2(transform.position.x - ledgeDetectionWidth, transform.position.y), new Vector2(0, -ledgeDetectionDepth), Color.blue);
-
-            if (leftLedge.collider == null)
-            {
-                direction = 1;
-            }
-        }
-        targetVelocity.x = direction * maxSpeed ;
     }
-
     public void Jump()
-    {
-        if (grounded)
         {
-            velocity.y = jumpTakeOffSpeed;
+            if (grounded)
+            {
+                velocity.y = jumpTakeOffSpeed;
+            }
         }
-    }
 }
+
+
 
